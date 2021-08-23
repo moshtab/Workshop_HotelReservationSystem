aa4edfc2332c2627b4f38e0e19065a5f1f6c7040
moshtab <mdmohsin41439@gmail.com>
2021-08-23 22:44:35 +0530

    Added all hotels and weekday rates

--- HotelReservationSystem.java
+++ HotelReservationSystem.java
@@ -1,35 +1,28 @@
 package workshop;
 
 import java.util.ArrayList;
+
 import java.util.List;
 
-class Hotels {
-	String name;
-	int weekdayRate;
-
-	public Hotels(String Nname, int num) {
-		this.name = Nname;
-		this.weekdayRate = num;
-	}
-}
-
 public class HotelReservationSystem {
+	static List<Hotels> list = new ArrayList<>();
+
+	// Displaying Hotels with its rates
+	public static void showHotels() {
+		for (Hotels i : list) {
+			System.out.println(i.name + " " + i.weekdayRate);
+		}
+	}
 
 	public static void main(String[] args) {
 		Hotels lakewood = new Hotels("Lakewood", 110);
 		Hotels bridgewood = new Hotels("Bridgewood", 160);
 		Hotels ridgewood = new Hotels("Ridgewood", 220);
 
-		List<Hotels> list = new ArrayList<>();
 		list.add(lakewood);
 		list.add(bridgewood);
 		list.add(ridgewood);
-
-		// Displaying Hotels with its rates
-		System.out.println("Showing Hotels and its weekday rates");
-		for (Hotels i : list) {
-			System.out.println(i.name + " " + i.weekdayRate);
-		}
+		showHotels();
 
 		// finding Cheapest hotels
 		if (lakewood.weekdayRate < bridgewood.weekdayRate && lakewood.weekdayRate < ridgewood.weekdayRate) {
---
+++ Hotels.java
@@ -0,0 +1,11 @@
+package workshop;
+
+public class Hotels {
+	String name;
+	int weekdayRate;
+
+	public Hotels(String Nname, int num) {
+		this.name = Nname;
+		this.weekdayRate = num;
+	}
+}
