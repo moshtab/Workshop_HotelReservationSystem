0739ccfddf8e1f7fea33fa96c21108909ba97ef5
moshtab <mdmohsin41439@gmail.com>
2021-08-23 18:34:48 +0530

    Added hotel Names and weekday Rates

---
+++ HotelReservationSystem.java
@@ -0,0 +1,33 @@
+package workshop;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Hotels {
+	String name;
+	int weekdayRate;
+
+	public Hotels(String Nname, int num) {
+		this.name = Nname;
+		this.weekdayRate = num;
+	}
+}
+
+public class HotelReservationSystem {
+
+	public static void main(String[] args) {
+		Hotels lakewood = new Hotels("Lakewood", 110);
+		Hotels bridgewood = new Hotels("Bridgewood", 160);
+		Hotels ridgewood = new Hotels("Ridgewood", 220);
+
+		List<Hotels> list = new ArrayList<>();
+		list.add(lakewood);
+		list.add(bridgewood);
+		list.add(ridgewood);
+
+		for (Hotels i : list) {
+			System.out.println(i.name + " " + i.weekdayRate);
+		}
+	}
+
+}
