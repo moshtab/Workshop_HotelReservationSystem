01261f64206e5d02f151a4839c8b7224d30e55b8
moshtab <mdmohsin41439@gmail.com>
2021-08-23 10:30:58 +0530

    Added hotels name and rates for regular customers

--- Hotel.java
+++ Hotel.java
@@ -1,10 +1,46 @@
 package workshop;
 
-public class Hotel {
+class Hotel {
+	public int rate;
+	private String hotelName;
+
+	public String getHotelName() {
+		return hotelName;
+	}
+
+	public void setHotelName(String string) {
+		this.hotelName = string;
+	}
+
+	public int getRate() {
+		return rate;
+	}
+
+	public void setRate(int num) {
+		this.rate = num;
+	}
+	
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Hotel Reservation System");
 
+		Hotel lakewood = new Hotel();
+		lakewood.setHotelName("Lakewood");
+		lakewood.setRate(110);
+		System.out.println("Hotel Name is" + lakewood.getHotelName());
+		System.out.println("Rate for Regular customer is : " + lakewood.getRate());
+
+		Hotel bridgewood = new Hotel();
+		bridgewood.setHotelName("Bridgewood");
+		bridgewood.setRate(160);
+		System.out.println("Hotel Name is" + bridgewood.getHotelName());
+		System.out.println("Rate for Regular customer is :" + bridgewood.getRate());
+
+		Hotel ridgewood = new Hotel();
+		ridgewood.setHotelName("Bridgewood");
+		ridgewood.setRate(220);
+		System.out.println("Hotel Name is" + ridgewood.getHotelName());
+		System.out.println("Rate for Regular customer is : " + ridgewood.getRate());
 	}
 
 }
