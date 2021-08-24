03493fa2541eb97bd22caf04ee83f4bd54991d10
moshtab <87015799+moshtab@users.noreply.github.com>
2021-08-24 12:44:31 +0530

    Update HotelReservationSystem.java

--- HotelReservationSystem.java
+++ HotelReservationSystem.java
@@ -1,46 +1,63 @@
 package workshop;
 
+import java.time.LocalDate;
+
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
-
-class Hotels {
-	String name;
-	int weekdayRate;
-
-	public Hotels(String Nname, int num) {
-		this.name = Nname;
-		this.weekdayRate = num;
-	}
-}
+import java.util.Scanner;
 
 public class HotelReservationSystem {
+	static List<Hotels> list = new ArrayList<>();
+
+	// Displaying Hotels with its rates
+	public static void showHotels() {
+		for (Hotels i : list) {
+			System.out.println(i.name + " " + i.weekdayRate);
+		}
+
+	}
+
+	// finding Cheapest hotels
+	private static void findCheapestHotel(int lakwoodWeekdayRate, int briwoodWeekdayRate, int ridgewodWeekdayRate) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter start date");
+		String startDate1 = s.nextLine();
+		System.out.println(startDate1);
+		System.out.println("Enter end date");
+		String endDate2 = s.nextLine();
+		DateTimeFormatter formatDate = DateTimeFormatter.ofPattern("ddMMyyyy");
+		LocalDate startDate = LocalDate.parse(startDate1, formatDate);
+		LocalDate endDate = LocalDate.parse(endDate2, formatDate);
+		long days = ChronoUnit.DAYS.between(startDate, endDate);
+		System.out.println(days);
+		s.close();
+
+		long lakewoodWeekdayRate = days * lakwoodWeekdayRate;
+		long bridgewoodWeekdayRate = days * briwoodWeekdayRate;
+		long ridgewoodWeekdayRate = days * ridgewodWeekdayRate;
+		if (lakewoodWeekdayRate < bridgewoodWeekdayRate && lakewoodWeekdayRate < ridgewoodWeekdayRate) {
+			System.out.println("Cheapest Hotel is  lakewood and rate is " + lakewoodWeekdayRate);
+
+		} else if (bridgewoodWeekdayRate < lakewoodWeekdayRate && bridgewoodWeekdayRate < ridgewoodWeekdayRate) {
+			System.out.println("Cheapest Hotel is bridgewoodand rate is " + bridgewoodWeekdayRate);
+		} else {
+			System.out.println("Cheapest Hotel is ridgewood and rate is " + ridgewoodWeekdayRate);
+		}
+	}
 
 	public static void main(String[] args) {
 		Hotels lakewood = new Hotels("Lakewood", 110);
 		Hotels bridgewood = new Hotels("Bridgewood", 160);
 		Hotels ridgewood = new Hotels("Ridgewood", 220);
 
-		List<Hotels> list = new ArrayList<>();
 		list.add(lakewood);
 		list.add(bridgewood);
 		list.add(ridgewood);
 
-		// Displaying Hotels with its rates
-		System.out.println("Showing Hotels and its weekday rates");
-		for (Hotels i : list) {
-			System.out.println(i.name + " " + i.weekdayRate);
-		}
-
-		// finding Cheapest hotels
-		if (lakewood.weekdayRate < bridgewood.weekdayRate && lakewood.weekdayRate < ridgewood.weekdayRate) {
-			System.out.println("Cheapest Hotel is " + lakewood.name + "and rate is " + lakewood.weekdayRate);
-
-		} else if (bridgewood.weekdayRate < lakewood.weekdayRate && bridgewood.weekdayRate < ridgewood.weekdayRate) {
-			System.out.println("Cheapest Hotel is " + bridgewood.name + "and rate is " + bridgewood.weekdayRate);
-		} else {
-			System.out.println("Cheapest Hotel is " + ridgewood.name + "and rate is " + ridgewood.weekdayRate);
-		}
+		showHotels();
+		findCheapestHotel(lakewood.weekdayRate, bridgewood.weekdayRate, ridgewood.weekdayRate);
 
 	}
-
 }
