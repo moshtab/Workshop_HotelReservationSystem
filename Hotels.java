1cb6110255b87181e46dd2b0190d003795d6b276
moshtab <87015799+moshtab@users.noreply.github.com>
2021-08-24 12:43:21 +0530

    Create Hotels.java

---
+++ Hotels.java
@@ -0,0 +1,11 @@
+package workshop;
+
+public class Hotels {
+	public String name;
+	public int weekdayRate;
+
+	public Hotels(String Nname, int num) {
+		this.name = Nname;
+		this.weekdayRate = num;
+	}
+}
